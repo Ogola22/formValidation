df97437920fbdcd985ead29ac41c1bd37e747718
Brian Ogola <brianochiengogola78@gmail.com>
2023-01-17 22:11:25 +0300

    stylesheet file

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
             <div class="card-body">
                 <h5 class="card-title">Form Validation project using PHP.</h5>
                 <p class="card-text">I'm Brian, currently learning PHP programming Language. I find it easy and interesting to learn PHP. I'm currently working on a form validation project.</p>
-                <a href="register.html" class="btn btn-primary">Let's see this</a>
+                <a href="register.html" class="btn btn-primary">Let's see this...</a>
             </div>
         </div>
        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
