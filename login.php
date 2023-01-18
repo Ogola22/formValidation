bf0c963833bc48a096c4aa0712e1ed1b6878d789
Brian Ogola <brianochiengogola78@gmail.com>
2023-01-18 09:39:53 +0300

    stylinng forms

--- login.php
+++ login.php
@@ -6,9 +6,8 @@
         <title>Form validation</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel ="stylesheet" href ="styles.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
+        <link rel ="stylesheet" href ="styles.css">
     </head>
     <body>
         <nav class="navbar navbar-expand-lg bg-light">
@@ -32,21 +31,23 @@
             </div>
             </div>
         </nav>
+    <div class ="container">
         <form>
             <div class="row mb-3">
                 <label for="inputUsername" class="col-sm-2 col-form-label">Username</label>
-                <div class="col-sm-10">
+                <div class="col-sm-5">
                     <input type="email" class="form-control" id="inputEmail3">
                 </div>
             </div>
             <div class="row mb-3">
             <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                <div class="col-sm-10">
+                <div class="col-sm-5">
                     <input type="password" class="form-control" id="inputPassword3">
                 </div>
             </div>
            <button type="submit" class="btn btn-primary">Log in</button>
         </form>
+    </div>
        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -6,19 +6,20 @@
         <title>Form validation</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel ="stylesheet" href ="styles.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
+        <link rel ="stylesheet" href ="styles.css">
         <?php
         //checking for firstname validation
-        if(empty($_POST['firstname'];)){
-            $errors['firstname'] = 'First name cannot be empty'<br>;
+        if(empty($_POST['firstname'])){
+            $errors['firstname'] = 'First name cannot be empty <br>';
         }else{
             $firstname = $_POST['firstname'];
             if(!preg_match('/^[a-zA-Z\s]+$/',$firstname)){
                 $errors['firstname']='First name must be letters and spaces only';
             }
         }
+        ?>
     </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-light">
@@ -42,33 +43,35 @@
         </div>
         </div>
     </nav>
+    <div class ="container">
     <form>
         <div class="row mb-3">
             <label for="firstname" class="col-sm-2 col-form-label">First name</label>
-            <div class="col-sm-10">
+            <div class="col-sm-5">
                 <input type="text" class="form-control" id="firstname">
             </div>
         </div>
         <div class="row mb-3">
             <label for="lastname" class="col-sm-2 col-form-label">Last name</label>
-            <div class="col-sm-10">
+            <div class="col-sm-5">
                 <input type="text" class="form-control" id="lastname">
             </div>
         </div>
         <div class="row mb-3">
             <label for="email" class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-10">
+            <div class="col-sm-5">
                 <input type="email" class="form-control" id="email">
             </div>
         </div>
         <div class="row mb-3">
         <label for="password" class="col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-10">
+            <div class="col-sm-5">
                 <input type="password" class="form-control" id="password">
             </div>
         </div>
        <button type="submit" class="btn btn-primary">Sign in</button>
     </form>
+    </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
